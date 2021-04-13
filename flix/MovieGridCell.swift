6dedc50903e3f0ba2d77796a6fd15757998321dc
Jason <jasonyu112@gmail.com>
2021-04-12 22:23:30 -0700

    Unit 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Jason Yu on 4/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+}
